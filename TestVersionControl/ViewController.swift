be7f98ebe77acc659c78bfde0f4abbc7798c03ba
Saravanan R <saravananesec@gmail.com>
2021-11-05 19:07:51 +0530

    Function Implemented

--- TestVersionControl/ViewController.swift
+++ TestVersionControl/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.displayUploadImages()
+    }
+    
+    func displayUploadImages() {
+        print("Upload Imgaes")
     }
 
 
